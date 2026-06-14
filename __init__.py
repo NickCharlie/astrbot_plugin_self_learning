05165f709accdba79e026f04d4b7639733b352cb
EterUltimate <1831303476@qq.com>
2026-06-14 21:38:28 +0800

    chore: bump release to 3.4.0

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 # AstrBot 自学习插件
-__version__ = "3.3.0"
+__version__ = "3.4.0"
 
 # Ensure parent namespace packages ("data", "data.plugins") are
 # durably registered in sys.modules.  AstrBot loads plugins via
